6380173bb0bfa826cc10df40a401fff49c23ffde
Thomas Gourlaouen <68146526+ThomasG14@users.noreply.github.com>
2022-11-30 12:20:00 +0100

    V1 final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 let button = document.getElementById("button");
 let image = document.getElementById("image");
-let pokeNumber = document.getElementById("number");
 let pokeName = document.getElementById("name");
 
 let type = document.getElementById("type");
@@ -13,26 +12,41 @@ let exp = document.getElementById("exp");
 
 
 const changePokemon = async () => {
-    let randomNumber = Math.ceil(Math.random() * 150) + 1; // 1 et 151
+    let randomNumber = Math.ceil(Math.random() * 150) + 1;
 
     let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
 
     let data = await fetch(requestString);
-    console.log(data);
-
     let response = await data.json();
-    console.log(response);
+    //console.log(response);
 
     image.src = response.sprites.front_default;
-    pokeName.textContent = response.name;
 
-    type.textContent = response.types[0].type.name;
+    let name = response.name;
+    pokeName.textContent = name.toUpperCase();
 
-    hp.textContent = response.stats[0].base_stat;
-    def.textContent = response.stats[1].base_stat;
-    atk.textContent = response.stats[2].base_stat;
-    spd.textContent = response.stats[5].base_stat;
-    exp.textContent = response.base_experience;
+    let typ = response.types[0].type.name;
+    type.textContent = typ.toUpperCase();
+
+    let hplvl = response.stats[0].base_stat;
+    hp.textContent = hplvl + "/ 300";
+    hp.style.width = hplvl;
+
+    let deflvl = response.stats[1].base_stat;
+    def.textContent = deflvl + "/ 300"
+    def.style.width = deflvl;
+
+    let atklvl = response.stats[2].base_stat;
+    atk.textContent = atklvl + "/ 300"
+    atk.style.width = atklvl;
+
+    let spdlvl = response.stats[5].base_stat;
+    spd.textContent = spdlvl + "/ 300"
+    spd.style.width = spdlvl;
+
+    let explvl = response.base_experience;
+    exp.textContent = explvl + "/ 1000"
+    exp.style.width = explvl / 3.45;
 };
 
 changePokemon();
